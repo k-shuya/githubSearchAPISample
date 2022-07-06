ee8bad45493bba01372b354f6eded1433c5025de
k-shuya <kwamua@me.com>
2022-07-06 21:29:36 +0900

    Initial Commit

---
+++ githubSearchAPISample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  githubSearchAPISample
+//
+//  Created by 川村周也 on 2022/07/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ githubSearchAPISample/githubSearchAPISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubSearchAPISampleApp.swift
+//  githubSearchAPISample
+//
+//  Created by 川村周也 on 2022/07/06.
+//
+
+import SwiftUI
+
+@main
+struct githubSearchAPISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
