ef85f4d5d562d4ea35e5a00b9198d187b73cb3ea
k-shuya <kwamua@me.com>
2022-07-07 19:58:47 +0900

    add soucery templates

---
+++ githubSearchAPISample/Sourcery/Sources/SearchUserSource.swift
@@ -0,0 +1,11 @@
+//
+//  SearchUser.swift
+//  githubSearchAPISample
+//
+//  Created by 川村周也 on 2022/7/07.
+//  Copyright © 2022 川村周也 All rights reserved.
+//
+
+import Foundation
+
+protocol SearchUser: AutoViewControllable, AutoPresentable, AutoInteractable, AutoRoutable {}
