e0c12a3f9a1db5c7090f394cc723bcdee6fc56a8
k-shuya <kwamua@me.com>
2022-07-11 01:57:13 +0900

    sourceryのテンプレートを追加

--- githubSearchAPISample/Sourcery/Sources/SearchUserSource.swift
+++ githubSearchAPISample/Sourcery/Sources/SearchUserSource.swift
@@ -3,14 +3,8 @@
 //  githubSearchAPISample
 //
 //  Created by 川村周也 on 2022/7/07.
-//  Copyright © 2022 川村周也 All rights reserved.
 //
 
 import Foundation
-
-typealias AutoViewControllable = Any
-typealias AutoPresentable = Any
-typealias AutoInteractable = Any
-typealias AutoRoutable = Any
 
 protocol SearchUser: AutoViewControllable, AutoPresentable, AutoInteractable, AutoRoutable {}
