5b00f77f240a70fe8c9ac65807d3ec7167ffd9a6
k-shuya <kwamua@me.com>
2022-07-10 23:54:09 +0900

    ユーザ検索画面のVCを修正

--- githubSearchAPISample/View/SearchUserVC.swift
+++ githubSearchAPISample/View/SearchUserVC.swift
@@ -11,15 +11,64 @@
 
 import UIKit
 
+protocol SearchUserView: AnyObject {
+    func reloadTableView(items: [GithubSearchUserEntity])
+    func showErrorAlert()
+}
+
 final class SearchUserVC: UIViewController {
 
-    var presenter: SearchUserPresenterInterface!
+    var presenter: SearchUserPresentation!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchresultTable: UITableView!
+    
+    private var repositories: [GithubSearchUserEntity] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.searchresultTable.reloadData() // 画面の更新
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchBar.delegate = self
+        searchresultTable.delegate = self
+    }
+    
+    func inject(presenter: SearchUserPresentation) {
+        self.presenter = presenter
     }
 }
 
-extension SearchUserVC {
-    // 追加
+extension SearchUserVC: SearchUserView {
+    func reloadTableView(items: [GithubSearchUserEntity]) {
+        DispatchQueue.main.async {
+            self.searchresultTable.reloadData()
+        }
+    }
+}
+
+extension SearchUserVC: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text else { return }
+
+        presenter.searchButtonDidPush(searchText: text)
+
+        searchBar.resignFirstResponder()
+    }
+    
+    func showErrorAlert() {
+        let alert = UIAlertController(title: "ネットワークエラー", message: "しばらく時間をおいてから再度お試しください", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        DispatchQueue.main.async {
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+}
+
+extension SearchUserVC: UITableViewDelegate {
+    
 }
