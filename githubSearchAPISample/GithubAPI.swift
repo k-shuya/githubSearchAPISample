f387119ea8a2162cbc03c1521c145c286d4c29d8
k-shuya <kwamua@me.com>
2022-07-10 23:57:04 +0900

    APIのリクエスト処理を修正

--- githubSearchAPISample/GithubAPI.swift
+++ githubSearchAPISample/GithubAPI.swift
@@ -10,18 +10,19 @@ import Alamofire
 typealias ResultHandler<T> = (Result<T, GithubAPIError>) -> Void
 
 struct GithubAPI {
-    func request(handler: @escaping ResultHandler<GithubSearchUserItems>) {
+    func requestSearchUser(keyword: String, handler: @escaping ResultHandler<[GithubSearchUserEntity]>) {
         if URL(string: GithubAPIEndpoints.searchUser) == nil {
             handler(.failure(.invalidURL))
             return
         }
-        AF.request(GithubAPIEndpoints.searchUser).responseData { response in
+
+        AF.request(GithubAPIEndpoints.searchUser + "?q=" + keyword).responseData { response in
             guard let data = response.data else {
                 handler(.failure(.invalidResponse))
                 return
             }
             do {
-                let items = try JSONDecoder().decode(GithubSearchUserItems.self, from: data)
+                let items = try JSONDecoder().decode([GithubSearchUserEntity].self, from: data)
                 handler(.success(items))
             } catch {
                 handler(.failure(.unknown(error)))
