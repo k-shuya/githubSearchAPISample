5d6fa4486e0e9518917a70abb78e63e1c969f13f
k-shuya <kwamua@me.com>
2022-07-10 20:08:04 +0900

    add Const.swift

---
+++ githubSearchAPISample/Const.swift
@@ -0,0 +1,12 @@
+//
+//  Const.swift
+//  githubSearchAPISample
+//
+//  Created by 川村周也 on 2022/07/09.
+//
+
+import Foundation
+
+public struct GithubAPIEndpoints {
+    public static let searchUser = "https://api.github.com/search/users"
+}
