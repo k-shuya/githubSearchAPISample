a48a7147be303ad2584bab6e8d55568f27a4c5fa
k-shuya <kwamua@me.com>
2022-07-10 20:26:38 +0900

    add SearchEntity

---
+++ githubSearchAPISample/Entity/GithubSearchUserEntity.swift
@@ -0,0 +1,26 @@
+//
+//  GithubSearchUserEntity.swift
+//  githubSearchAPISample
+//
+//  Created by 川村周也 on 2022/07/08.
+//
+
+import Foundation
+
+struct GithubSearchUserItems: Codable {
+    let items: [GithubSearchUserEntity]
+}
+
+struct GithubSearchUserEntity: Codable {
+    let login: String
+    let type: String
+    let avatarUrl: String
+    let htmlUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case login = "login"
+        case type = "type"
+        case avatarUrl = "avatar_url"
+        case htmlUrl = "html_url"
+    }
+}
